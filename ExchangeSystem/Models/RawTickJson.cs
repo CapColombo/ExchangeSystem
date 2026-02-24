c87241370f8d22cbdcf57fc12a300aedfb8ec723
Александр Новиков <sachan311298@yandex.ru>
2026-02-24 19:06:22 +0500

    Добавлены методы Equals и GetHashCode в RawTickJson

--- ExchangeSystem/Models/RawTickJson.cs
+++ ExchangeSystem/Models/RawTickJson.cs
@@ -6,4 +6,16 @@ internal sealed class RawTickJson
     public decimal Price { get; init; }
     public decimal Volume { get; init; }
     public DateTime TimeStamp { get; init; }
+
+    private bool Equals(RawTickJson other) => 
+        Ticker == other.Ticker && 
+        Price == other.Price && 
+        Volume == other.Volume && 
+        TimeStamp.Equals(other.TimeStamp);
+
+    public override bool Equals(object? obj) => 
+        ReferenceEquals(this, obj) || obj is RawTickJson other && Equals(other);
+
+    public override int GetHashCode() => 
+        HashCode.Combine(Ticker, Price, Volume, TimeStamp);
 }
